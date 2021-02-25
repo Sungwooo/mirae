8a145e57cdf89ac596926e48116b8e5cd728695b
mukjo96 <mukjo96@gmail.com>
2021-02-25 17:42:03 +0900

    Update BottomDrawerMenu

--- lib/map.dart
+++ lib/map.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:mirae/map/menu_widget.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -28,6 +29,119 @@ class _MapPageState extends State<MapPage> {
 
   Set<Marker> _markers = Set.from([]);
 
+  bool showBottomMenu = false;
+
+  @override
+  void dispose() {
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loading = true;
+    toggleBtn = true;
+    _getMyLocation();
+    _markers = Set.from([]);
+    setCustomMapPin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    LatLng pinPostion = LatLng(35.149310, 129.063990);
+    LatLng myPosition =
+        LatLng(_currentPosition.latitude, _currentPosition.longitude);
+
+/*     var threshold = 100;
+ */
+    return Scaffold(
+        appBar: CupertinoNavigationBar(
+          middle: Text(
+            "MAP",
+            style:
+                TextStyle(color: Color(0xff31AC53), fontFamily: 'GoogleSans'),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: _currentPosition != null && loading == false
+            ? Stack(children: <Widget>[
+                Container(
+                  width: width,
+                  height: height,
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: myPosition,
+                      zoom: 18,
+                    ),
+                    myLocationButtonEnabled: false,
+                    markers: (marker != null)
+                        ? _markers.union(Set.of([marker]))
+                        : _markers,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller = controller;
+                      setState(() {
+                        _markers.add(Marker(
+                            markerId: MarkerId('<MARKER_1>'),
+                            position: pinPostion,
+                            icon: pinLocationIcon));
+                        _markers.add(Marker(
+                            markerId: MarkerId('<MARKER_2>'),
+                            position: LatLng(35.150310, 129.063990),
+                            icon: pinLocationIcon));
+                        _markers.add(Marker(
+                            markerId: MarkerId('<MARKER_3>'),
+                            position: LatLng(35.149310, 129.063490),
+                            icon: pinLocationIcon));
+                      });
+                    },
+                  ),
+                ),
+                /* GestureDetector(
+                    onPanEnd: (details) {
+                      if (details.velocity.pixelsPerSecond.dy > threshold) {
+                        this.setState(() {
+                          showBottomMenu = false;
+                        });
+                      } else if (details.velocity.pixelsPerSecond.dy <
+                          -threshold) {
+                        this.setState(() {
+                          showBottomMenu = true;
+                        });
+                      }
+                    },
+                    child:  */
+                AnimatedPositioned(
+                    curve: Curves.easeInOut,
+                    duration: Duration(milliseconds: 200),
+                    left: 0,
+                    bottom: (showBottomMenu) ? -60 : -(height * 4 / 9) + 25,
+                    child: MenuWidget()),
+              ])
+            : null,
+        floatingActionButton: FloatingActionButton.extended(
+            icon: toggleBtn
+                ? Icon(Icons.location_disabled)
+                : Icon(Icons.location_searching),
+            label: Text(''),
+            backgroundColor: Colors.white,
+            onPressed: () {
+              getCurrentLocation();
+              toggleBtn = !toggleBtn;
+            }),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endTop);
+  }
+
   Future<Uint8List> getMarker() async {
     ByteData byteData = await DefaultAssetBundle.of(context)
         .load("assets/icon/map/myMarker.png");
@@ -40,7 +154,6 @@ class _MapPageState extends State<MapPage> {
       marker = Marker(
           markerId: MarkerId("home"),
           position: latlng,
-          rotation: newLocalData.heading,
           draggable: false,
           zIndex: 2,
           flat: true,
@@ -67,7 +180,7 @@ class _MapPageState extends State<MapPage> {
         _locationSubscription.cancel();
       }
 
-      if (toggleBtn) {
+      if (!toggleBtn) {
         _locationSubscription =
             _locationTracker.onLocationChanged.listen((newLocalData) {
           if (_controller != null) {
@@ -105,84 +218,10 @@ class _MapPageState extends State<MapPage> {
     }
   }
 
-  @override
-  void dispose() {
-    if (_locationSubscription != null) {
-      _locationSubscription.cancel();
-    }
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    loading = true;
-    toggleBtn = true;
-    _getMyLocation();
-    _markers = Set.from([]);
-    setCustomMapPin();
-  }
-
   void setCustomMapPin() async {
     pinLocationIcon = await BitmapDescriptor.fromAssetImage(
         ImageConfiguration(devicePixelRatio: 2.5),
         "assets/icon/map/trashMarker.png");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    LatLng pinPostion = LatLng(35.149310, 129.063990);
-    LatLng myPosition =
-        LatLng(_currentPosition.latitude, _currentPosition.longitude);
-    return Scaffold(
-      appBar: CupertinoNavigationBar(
-        middle: Text(
-          "MAP",
-          style: TextStyle(color: Color(0xff31AC53), fontFamily: 'GoogleSans'),
-        ),
-      ),
-      body: _currentPosition != null && loading == false
-          ? Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: GoogleMap(
-                initialCameraPosition: CameraPosition(
-                  target: myPosition,
-                  zoom: 18,
-                ),
-                myLocationButtonEnabled: false,
-                markers: (marker != null)
-                    ? _markers.union(Set.of([marker]))
-                    : _markers,
-                onMapCreated: (GoogleMapController controller) {
-                  _controller = controller;
-                  setState(() {
-                    _markers.add(Marker(
-                        markerId: MarkerId('<MARKER_1>'),
-                        position: pinPostion,
-                        icon: pinLocationIcon));
-                    _markers.add(Marker(
-                        markerId: MarkerId('<MARKER_2>'),
-                        position: LatLng(35.150310, 129.063990),
-                        icon: pinLocationIcon));
-                    _markers.add(Marker(
-                        markerId: MarkerId('<MARKER_3>'),
-                        position: LatLng(35.149310, 129.063490),
-                        icon: pinLocationIcon));
-                  });
-                },
-              ),
-            )
-          : null,
-      floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.location_searching),
-          label: Text(''),
-          backgroundColor: toggleBtn ? Colors.white : Colors.grey,
-          onPressed: () {
-            getCurrentLocation();
-            toggleBtn = !toggleBtn;
-          }),
-    );
   }
 
   _getMyLocation() async {
