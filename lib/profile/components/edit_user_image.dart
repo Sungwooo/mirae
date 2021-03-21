db7fa1aaf143e841870a44fea8b9f3d9e167a796
mukjo96 <mukjo96@gmail.com>
2021-03-21 14:25:55 +0900

    Fix ios storage setting

--- lib/profile/components/edit_user_image.dart
+++ lib/profile/components/edit_user_image.dart
@@ -14,37 +14,43 @@ class EditImageNameWidget extends StatefulWidget {
 }
 
 class _EditImageNameWidgetState extends State<EditImageNameWidget> {
-
   FirebaseProvider fp;
   FirebaseUser _user;
 
-  Image profileImage;
+  String _userPhotoUrl;
   FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
 
   void initState() {
     super.initState();
   }
 
-
-
   Widget build(BuildContext context) {
     fp = Provider.of<FirebaseProvider>(context, listen: true);
     double width = MediaQuery.of(context).size.width;
-    _user=fp.getUser();
+    _user = fp.getUser();
     return Container(
       transform: Matrix4.translationValues(0.0, -50.0, 0.0),
       child: Column(children: [
-          InkWell(
-          onTap: () => _uploadImageToStorage(ImageSource.gallery),
-              child: CircleAvatar(
-                radius: width * 0.12,
-                backgroundColor: Colors.white,
-                child: CircleAvatar(
-                  radius: (width * 0.12) - 2,
-                  backgroundImage:  (profileImage != null) ? profileImage : NetworkImage(""),
-                ),
-              ),
+        InkWell(
+          onTap: () {
+            _uploadImageToStorage(ImageSource.gallery);
+            setState(() {
+              _user = fp.getUser();
+              _userPhotoUrl = _user.photoUrl;
+            });
+          },
+          child: CircleAvatar(
+            radius: width * 0.12,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+                radius: (width * 0.12) - 2,
+                backgroundImage: _userPhotoUrl != null
+                    ? NetworkImage(_userPhotoUrl)
+                    : _user.photoUrl != null
+                        ? NetworkImage(_user.photoUrl)
+                        : Image.asset("assets/EditProfileImage.png")),
           ),
+        ),
       ]),
     );
   }
@@ -56,16 +62,16 @@ class _EditImageNameWidgetState extends State<EditImageNameWidget> {
     if (image == null) return;
 
     StorageReference storageReference =
-    _firebaseStorage.ref().child("profile/${_user.uid}");
+        _firebaseStorage.ref().child("profile/${_user.uid}");
 
     StorageUploadTask storageUploadTask = storageReference.putFile(image);
 
     String downloadURL = await storageReference.getDownloadURL();
     UserUpdateInfo userUpdateInfo = UserUpdateInfo();
-    userUpdateInfo.photoUrl=downloadURL;
+    userUpdateInfo.photoUrl = downloadURL;
     await _user.updateProfile(userUpdateInfo);
+    print(fp.getUser().photoUrl);
 
     await storageUploadTask.onComplete;
-
   }
 }
