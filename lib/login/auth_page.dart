b6f8eeaab36b528502b2470faf46f137ed8a4ed5
kdhmny@naver.com <rlaejrgus3#>
2021-03-25 15:41:08 +0900

    signup member register

--- lib/login/auth_page.dart
+++ lib/login/auth_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:mirae/global.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +18,7 @@ class AuthPage extends StatefulWidget {
 }
 
 class AuthPageState extends State<AuthPage> {
+  final databaseReference = FirebaseDatabase.instance.reference().child('user');
   FirebaseProvider fp;
 
   @override
@@ -25,9 +27,36 @@ class AuthPageState extends State<AuthPage> {
 
     if (fp.getUser() != null) {
       Globals.changeUid(fp.getUser().uid);
+      if(searchData() == null){
+        createData();
+      }
       return MainPage(cameras);
     } else {
       return LogIn();
     }
   }
+
+  // ignore: missing_return
+  Future<bool> searchData() async {
+    try {
+      fp = Provider.of<FirebaseProvider>(context);
+      databaseReference.child(fp.getUser().uid).once().then((DataSnapshot snapshot) {
+        return true;
+      });
+    } on Exception catch (e) {
+
+    }
+  }
+
+  void createData() {
+    databaseReference.child(fp.getUser().uid).set({
+      'name': fp.getUser().displayName,
+      'country': 'US',
+      'discard': 0,
+      'point': 0,
+      'ping': 0,
+      'ImageUrl': 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAzMDdfMTQ2%2FMDAxNjE1MDk3MjM1MDY4.8Vv_CZkS8JeQmLyZ9hZ-v1Xl_g34aOftc-Ig0hKMuuog.OPqz0oZcuR_5oekb2Dgw0s3Yt2rtdS4zvPtP2E_4WsQg.JPEG.eeeuz%2F%25B9%25AB%25BE%25DF%25C8%25A3%25C2%25A93.jpg&type=sc960_832'
+    });
+  }
 }
+
